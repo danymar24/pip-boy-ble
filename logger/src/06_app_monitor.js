91a9b16ae73256a83f4f6f57652b6d46fca52657
Daniel Rodriguez <danymar24@gmail.com>
2026-03-27 12:01:08 -0500

    Adjust max logs to 10

--- logger/src/06_app_monitor.js
+++ logger/src/06_app_monitor.js
@@ -3,7 +3,7 @@
 // ==========================================
 Pip.serialLogs = ["UPLINK MONITOR INITIALIZED", "Awaiting Serial data..."];
 Pip.isMonitorActive = false;
-Pip.maxLogs = 11;
+Pip.maxLogs = 10;
 
 Pip.logMonitor = function (msg) {
     var cleanMsg = msg.replace(/\r/g, "").replace(/\n/g, "");
